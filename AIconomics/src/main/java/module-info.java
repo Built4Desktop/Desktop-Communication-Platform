aa27bb923b6bad5560cb511bbe5d91c44a947d04
Robin Ringwelski <94263613+RobinRingwelski@users.noreply.github.com>
2025-03-03 17:38:26 +0100

    Delete AIconomics directory

--- AIconomics/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.aiconomics {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.aiconomics to javafx.fxml;
-    exports org.example.aiconomics;
-}
